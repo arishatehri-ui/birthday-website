5a5750e57bb8055ed9d539f0cc4866739705eb25
arishatehri-ui <arishatehri@gmail.com>
2026-06-29 22:18:30 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
 document.getElementById("startBtn").addEventListener("click", function () {
-
   const cover = document.querySelector(".cover");
 
   cover.classList.add("hide");
 
   setTimeout(() => {
-
     cover.innerHTML = `
-
       <h1>Hi Birthday Boy. 🤍</h1>
-
-      <p>I couldn't wrap this in paper...
-
-      so I wrapped it in code.</p>
-
+      <p>I couldn't wrap this in paper... so I wrapped it in code.</p>
       <button id="nextBtn">Next →</button>
-
     `;
 
     cover.classList.remove("hide");
-
-  }, 600);
-
+  }, 500);
 });
-    
